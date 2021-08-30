cae0c382859d3c7846d56a4bd6a9af388a6d7e61
mengke <mk95816@163.com>
2021-08-30 17:00:17 +0800

    first commit

---
+++ src/main/java/com/mk/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.mk.git;
+
+/**
+ * @Author MengKe
+ * @Date 2021/8/30 16:52
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("fuck you,git");
+    }
+}
